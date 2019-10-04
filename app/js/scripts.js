98820ff78a890cfb0cdf19f6dd99a60797572e50
Sergey Nacharov <xkoredeus@gmail.com>
2019-10-04 10:45:52 +0400

    feat: banner

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -22,8 +22,86 @@ $(function() {
             items: 4
         }
       }
-	});
-	// tabs 
+  });
+  //card slider
+  var sync1 = $('.banner__bg');
+  var sync2 = $('.banner__slider');
+
+  var thumbnailItemClass = '.owl-item';
+
+  var slides = sync1.owlCarousel({
+    startPosition: 0,
+    items:1,
+    loop:false,
+    margin:10,
+    nav: false,
+    navText: ["<img src='img/slider__arrow_prev.svg'>", "<img src='img/slider__arrow_next.svg'>"],
+    dots: false,
+    smartSpeed: 700,
+    autoplay:false,
+    autoplayTimeout:6000,
+    autoplayHoverPause:false,
+    mouseDrag: false,
+  }).on('changed.owl.carousel', syncPosition);
+
+  function syncPosition(el) {
+    $owl_slider = $(this).data('owl.carousel');
+    var loop = $owl_slider.options.loop;
+
+    if(loop){
+      var count = el.item.count-1;
+      var current = Math.round(el.item.index - (el.item.count/2) - .5);
+      if(current < 0) {
+          current = count;
+      }
+      if(current > count) {
+          current = 0;
+      }
+    }else{
+      var current = el.item.index;
+    }
+
+    var owl_thumbnail = sync2.data('owl.carousel');
+    var itemClass = "." + owl_thumbnail.options.itemClass;
+
+
+    var thumbnailCurrentItem = sync2
+    .find(itemClass)
+    .removeClass("synced")
+    .eq(current);
+
+    thumbnailCurrentItem.addClass('synced');
+
+    if (!thumbnailCurrentItem.hasClass('active')) {
+      var duration = 300;
+      sync2.trigger('to.owl.carousel',[current, duration, true]);
+    }   
+  }
+  var thumbs = sync2.owlCarousel({
+    startPosition: 0,
+    items:1,
+    loop:false,
+    margin: 0,
+    autoplay:false,
+    nav: false,
+    dots: true,
+    mouseDrag: false,
+    onInitialized: function (e) {
+      var thumbnailCurrentItem =  $(e.target).find(thumbnailItemClass).eq(this._current);
+      thumbnailCurrentItem.addClass('synced');
+    },
+  })
+  .on('click', thumbnailItemClass, function(e) {
+      e.preventDefault();
+      var duration = 300;
+      var itemIndex =  $(e.target).parents(thumbnailItemClass).index();
+      sync1.trigger('to.owl.carousel',[itemIndex, duration, true]);
+  }).on("changed.owl.carousel", function (el) {
+    var number = el.item.index;
+    $owl_slider = sync1.data('owl.carousel');
+    $owl_slider.to(number, 100, true);
+  });
+  // tabs 
   if ( $(window).width() > 600 ) {
     $(document).ready(function () {
       $(".tabs__content-item:not(:first-child)").hide();
